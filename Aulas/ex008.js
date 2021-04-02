9f25e800ea0a6c9b9bab9f44c6536e93da07e686
Eneri da Costa Junior <jr.icm.sc@gmail.com>
2021-04-02 01:28:48 -0300

    Condicionais

---
+++ Aulas/ex008.js
@@ -0,0 +1,9 @@
+var velocidade = 12
+
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+
+if (velocidade > 60){
+    console.log(`Você ultrapassou a velocidade permitida, sua velocidade é ${velocidade} Você foi multado!`)
+}
+
+console.log("Dirija sempre usando cinto de segurança!")
