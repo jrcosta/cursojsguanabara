11a4d38ebef3547bc3c5eb6486613669aaa716b6
Eneri da Costa Junior <jr.icm.sc@gmail.com>
2021-04-06 22:47:28 -0300

    Ex interativo 2 concluido

--- Aulas/Ex_Site_Interativo_Idade/script.js
+++ Aulas/Ex_Site_Interativo_Idade/script.js
@@ -13,7 +13,41 @@ function verificar() {
     else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade Calculada: ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade <= 10){
+                //Criança
+                img.setAttribute('src', 'bebemenino.png')
+            } else if(idade < 21){
+                //Jovem
+                img.setAttribute('src', 'jovemmenino.png')
+            } else if(idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'homem.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'idoso.png')
+            }
+        } else {
+            genero = 'Mulher'
+            if(idade >= 0 && idade <= 10){
+                //Criança
+                img.setAttribute('src', 'bebemenina.png')
+            } else if(idade < 21){
+                //Jovem
+                img.setAttribute('src', 'jovemmenina.png')
+            } else if(idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'mulher.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `<p>Detectamos ${genero}  com ${idade} anos.</p>`
+        res.appendChild(img)
     }
-    res.innerHTML += `<p>Ano Digitado ${fano.value}</p>`
-}
