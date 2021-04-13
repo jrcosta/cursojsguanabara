a834444e62e0f606e3de7aeaba2a142b66b02612
Eneri da Costa Junior <jr.icm.sc@gmail.com>
2021-04-13 02:25:28 -0300

    Exercício Final

--- Aulas/Ex_Funcoes_Array/script.js
+++ Aulas/Ex_Funcoes_Array/script.js
@@ -1,4 +1,6 @@
 var n = []
+//var rodou = 0
+var dados = document.getElementById('dados')
 
 function listaNumeros() {
     let num = document.getElementById('txtn')
@@ -6,8 +8,10 @@ function listaNumeros() {
 
     if (num.value.length == 0) {
         window.alert('Por favor, Digite um número!')
-    } else if(num.value > 100 || num.value < 0 || num.value == n.indexOf(n)){
+    } else if(num.value > 100 || num.value < 0 || n.indexOf(num.value) == Number(num.value)){
         window.alert('Número inválido, por favor escolha outro')
+    /*} else if(rodou > 0){
+        dados.innerHTML.replace*/
     } else {
         n.push(Number(num.value))
         let c = 1
@@ -20,8 +24,12 @@ function listaNumeros() {
 } 
 
 function avalN() {
-    let dados = document.getElementById('dados')
-    dados.innerHTML += `<p>Ao todo temos ${n.length} numeros cadastrados.</p>`
+    const somar = (accumulator, currentValue) => accumulator + currentValue;
+    dados.innerHTML += `<p>Ao todo temos ${n.length} números cadastrados.</p>`
     dados.innerHTML += `<p>O maior número é ${Math.max.apply(null, n)}.</p>`
     dados.innerHTML += `<p>O menor número é ${Math.min.apply(null, n)}</p>`
+    dados.innerHTML += `<p>Somando todos os valores temos ${n.reduce(somar)}</p>`
+    dados.innerHTML += `<p>A média dos valores digitados é ${Number(n.reduce(somar))/Number(n.length)}</p>`
+    //rodou = 1
 }
+
